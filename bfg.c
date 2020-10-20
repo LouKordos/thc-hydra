c50202d10c64d6c15d99e061434ad0d16f67e4d5
Loukas K. (P53) <tuloukas@gmail.com>
2020-10-20 16:04:43 +0200

    Remove 4 billion combination limit, not even sure if this is dangerous

--- bfg.c
+++ bfg.c
@@ -189,11 +189,6 @@ uint64_t bf_get_pcount() {
 
   for (i = bf_options.from; i <= bf_options.to; i++)
     count += (pow((double)bf_options.crs_len, (double)i));
-  if (count >= 0xffffffff) {
-    fprintf(stderr, "\n[ERROR] definition for password bruteforce (-x) "
-                    "generates more than 4 billion passwords - this is not a bug in the program, it is just not feasible to try so many attempts. Try a calculator how long that would take. duh.\n");
-    exit(-1);
-  }
 
   foo = count / 1;
   return foo;
